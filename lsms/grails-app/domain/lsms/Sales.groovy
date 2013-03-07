3bc3e7411f3abd20d5d367994454d1b0ceee405b
aum_daganga <lyrexzalyzza@gmail.com>
2013-03-07 21:03:18 +0800

    added Daily, Weekly and Monthly functionality to Sales

--- lsms/grails-app/domain/lsms/Sales.groovy
+++ lsms/grails-app/domain/lsms/Sales.groovy
@@ -6,4 +6,22 @@ class Sales {
 
     static constraints = {
     }
+	
+	double getDaily() {
+		double result = 0;
+		customer.each()
+		result = it.customer.quantity * it.customPackage.price
+	}
+	
+	double getWeekly() {
+		double result = 0;
+		customer.each()
+		result = it.customer.quantity * it.customPackage.price * 7
+	}
+	
+	double getMonthly() {
+		double result = 0;
+		customer.each()
+		result = it.customer.quantity * it.customPackage.price * 30
+	}
 }
